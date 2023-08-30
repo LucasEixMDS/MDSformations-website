a01b402e1610b6f395d4a46fc0f08c9a358f9783
Eixarch Lucas <lucas.melodiedessens@gmail.com>
2023-08-30 16:37:11 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout"
+import Home from "./page/Home"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
