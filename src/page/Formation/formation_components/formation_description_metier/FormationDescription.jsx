749c39f14aa3579dc1ab7dfe143bf3a9b554dc5a
Eixarch Lucas <lucas.melodiedessens@gmail.com>
2024-01-17 09:54:35 +0100

    ajout pdf reflexo

--- src/page/Formation/formation_components/formation_description_metier/FormationDescription.jsx
+++ src/page/Formation/formation_components/formation_description_metier/FormationDescription.jsx
@@ -3,47 +3,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class FormationDescription extends React.Component {
-    handleDownloadClick = () => {
-      // Créez un élément <a> (lien)
-      const downloadLink = document.createElement('a');
-    
-      // Définissez l'URL du fichier que vous souhaitez télécharger
-      const fileUrl = 'URL_DU_FICHIER'; // Remplacez par l'URL réelle du fichier
-    
-      // Définissez le nom du fichier à télécharger
-      const fileName = 'nom-du-fichier.extension'; // Remplacez par le nom du fichier
-    
-      // Définissez l'attribut "href" avec l'URL du fichier
-      downloadLink.href = fileUrl;
-    
-      // Définissez l'attribut "download" avec le nom du fichier
-      downloadLink.download = fileName;
-    
-      // Simulez un clic sur le lien pour déclencher le téléchargement
-      downloadLink.click();
-    };
+  handleDownloadClick = () => {
+    // Créez un élément <a> (lien)
+    const downloadLink = document.createElement('a');
   
-    render() {
-      const { formation } = this.props;
+    // Définissez l'URL du fichier que vous souhaitez télécharger en utilisant formation.programme
+    const fileUrl = this.props.formation.programme; // Utilisation de la propriété formation.programme
   
-      return (
-        <div className="descriptionContainer">
-          <p>{formation.header.description}</p>
-          <p>{formation.header.description1}</p>
-          <p>{formation.header.description2}</p>
-          <p>{formation.header.description3}</p>
-          <p>{formation.header.description4}</p>
-          <p>{formation.header.description5}</p>
-          <button onClick={this.handleDownloadClick} style={{ backgroundColor: formation.colorFormation }}>
-            Télécharger le programme de formation
-          </button>
-        </div>
-      );
-    }
-  }
+    // Définissez le nom du fichier à télécharger
+    const fileName = 'nom-du-fichier.extension'; // Remplacez par le nom du fichier
   
-  FormationDescription.propTypes = {
-    formation: PropTypes.object.isRequired // Vous indiquez que 'formation' est un objet et est requis
+    // Définissez l'attribut "href" avec l'URL du fichier
+    downloadLink.href = fileUrl;
+  
+    // Définissez l'attribut "download" avec le nom du fichier
+    downloadLink.download = fileName;
+  
+    // Simulez un clic sur le lien pour déclencher le téléchargement
+    downloadLink.click();
   };
-  
-  export default FormationDescription;
+
+  render() {
+    const { formation } = this.props;
+
+    return (
+      <div className="descriptionContainer">
+        <p>{formation.header.description}</p>
+        <p>{formation.header.description1}</p>
+        <p>{formation.header.description2}</p>
+        <p>{formation.header.description3}</p>
+        <p>{formation.header.description4}</p>
+        <p>{formation.header.description5}</p>
+        <button onClick={this.handleDownloadClick} style={{ backgroundColor: formation.colorFormation }}>
+          Télécharger le programme de formation
+        </button>
+      </div>
+    );
+  }
+}
+
+FormationDescription.propTypes = {
+  formation: PropTypes.object.isRequired // Vous indiquez que 'formation' est un objet et est requis
+};
+
+export default FormationDescription;
