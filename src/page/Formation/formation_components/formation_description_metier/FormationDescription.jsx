6abca1d8d551198a5884b0289972c181e59b97dc
Eixarch Lucas <lucas.melodiedessens@gmail.com>
2024-01-17 09:28:20 +0100

    ajout telechargement

--- src/page/Formation/formation_components/formation_description_metier/FormationDescription.jsx
+++ src/page/Formation/formation_components/formation_description_metier/FormationDescription.jsx
@@ -1,22 +1,49 @@
-import "./formationDescription.scss"
+import "./formationDescription.scss";
+import React from 'react';
 import PropTypes from 'prop-types';
 
-function FormationDescription({formation}) {
-
-    return (
-        <div >
+class FormationDescription extends React.Component {
+    handleDownloadClick = () => {
+      // Créez un élément <a> (lien)
+      const downloadLink = document.createElement('a');
+    
+      // Définissez l'URL du fichier que vous souhaitez télécharger
+      const fileUrl = 'URL_DU_FICHIER'; // Remplacez par l'URL réelle du fichier
+    
+      // Définissez le nom du fichier à télécharger
+      const fileName = 'nom-du-fichier.extension'; // Remplacez par le nom du fichier
+    
+      // Définissez l'attribut "href" avec l'URL du fichier
+      downloadLink.href = fileUrl;
+    
+      // Définissez l'attribut "download" avec le nom du fichier
+      downloadLink.download = fileName;
+    
+      // Simulez un clic sur le lien pour déclencher le téléchargement
+      downloadLink.click();
+    };
+  
+    render() {
+      const { formation } = this.props;
+  
+      return (
+        <div className="descriptionContainer">
           <p>{formation.header.description}</p>
           <p>{formation.header.description1}</p>
           <p>{formation.header.description2}</p>
           <p>{formation.header.description3}</p>
           <p>{formation.header.description4}</p>
           <p>{formation.header.description5}</p>
+          <button onClick={this.handleDownloadClick} style={{ backgroundColor: formation.colorFormation }}>
+            Télécharger le programme de formation
+          </button>
         </div>
-    );
-}
-
-FormationDescription.propTypes = {
+      );
+    }
+  }
+  
+  FormationDescription.propTypes = {
     formation: PropTypes.object.isRequired // Vous indiquez que 'formation' est un objet et est requis
-};
-
-export default FormationDescription;
+  };
+  
+  export default FormationDescription;
