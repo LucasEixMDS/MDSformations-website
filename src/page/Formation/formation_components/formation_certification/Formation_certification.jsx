62bb2f912e91f60e8b441e14532eeb1757d2ac6b
Eixarch Lucas <lucas.melodiedessens@gmail.com>
2024-01-19 15:56:16 +0100

    ajout css desktop pour page formation bientot terminé

--- src/page/Formation/formation_components/formation_certification/Formation_certification.jsx
+++ src/page/Formation/formation_components/formation_certification/Formation_certification.jsx
@@ -19,15 +19,17 @@ const Formation_certification = ({ formation }) => {
             <h4>Optez pour <span style={colorStyle}>l'excellence et la crédibilité</span> en choisissant notre formation</h4>
             <div className="certification-container">
                 <p><img src={parchemin} alt="icon d'un parchemin" />Certification Reconnue : Obtenez un diplôme qui fait la différence et ouvre des portes dans votre domaine.</p>
+                <div className="certificationContainerDesktop">
                 <div className="certification-modal">
                     <h2 style={colorStyle}>{formation.certification.title}</h2>
                     <h3>{formation.certification.rncp}</h3>
                     <h3>{formation.certification.level}</h3>
                     <button style={ {backgroundColor: formation.colorFormation}}>En savoir +</button>
                 </div>
+                </div>
             </div>
-            <p><img src={cible} alt="icon d'un parchemin" />Avancez avec Confiance : Élevez votre expertise et votre carrière grâce à notre accréditation reconnue.</p>
-            <p><img src={trophy} alt="icon d'un parchemin" />Excellence Récompensée : Notre certification est le gage de votre engagement envers l'apprentissage continu et la maîtrise de votre domaine.</p>
+            <p className="certificationText"><img src={cible} alt="icon d'un parchemin" />Avancez avec Confiance : Élevez votre expertise et votre carrière grâce à notre accréditation reconnue.</p>
+            <p className="certificationText"><img src={trophy} alt="icon d'un parchemin" />Excellence Récompensée : Notre certification est le gage de votre engagement envers l'apprentissage continu et la maîtrise de votre domaine.</p>
         </div>
         <div className="apprentissage">
             <h4>Concentrez-vous sur votre apprentissage, <span style={colorStyle}>nous nous occupons du reste !</span></h4>
