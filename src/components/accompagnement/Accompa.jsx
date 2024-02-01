47a204d31f9d3c46863e7ecdc246fc7089a1966b
Eixarch Lucas <lucas.melodiedessens@gmail.com>
2024-02-01 16:38:24 +0100

    ajout component accompagnement

--- src/components/accompagnement/Accompa.jsx
+++ src/components/accompagnement/Accompa.jsx
@@ -8,8 +8,10 @@ const Accompa = () => {
                     <div className="modalIcon">
                         <p><img src="https://mdsformations-website-u5520.vm.elestio.app/selfie.svg" alt="icon apple d'une femme sur un pc" /></p>
                     </div>
-                    <h3>Besoin d'être accompagné ?</h3>
-                    <h4>Financements, plannings, débouchés...</h4>
+                    <div className="accompaText">
+                        <h3>Besoin d'être accompagné ?</h3>
+                        <h4>Financements, plannings, débouchés...</h4>
+                    </div>
                 </div>
                 <h3>Posez-nous vos questions ! </h3>
                 <button className="buttonAccompa">Prendre rdv par téléphone</button>
