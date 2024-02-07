496e6674027d2d0122ab086428128425ef122518
Eixarch Lucas <lucas.melodiedessens@gmail.com>
2024-02-07 16:03:44 +0100

    ajout de la page terminé devenir modele

--- src/page/Orientation/VAEVAP/Vae.jsx
+++ src/page/Orientation/VAEVAP/Vae.jsx
@@ -9,7 +9,7 @@ const Vae = () => {
                 <h1>La VAE et la VAP</h1>
                 <h2>Valorisez votre expérience dans le bien-être et l’esthétique</h2>
             </div>
-
+            <div className="vaePage">
             <p>Chez MDS Formations, nous comprenons que l'apprentissage ne suit pas toujours un chemin traditionnel. C'est pourquoi nous vous proposons deux opportunités précieuses pour mettre en valeur votre expérience et vos compétences dans le domaine du bien-être et de l'esthétique : la Validation des Acquis de l'Expérience (VAE) et la Validation des Acquis Professionnels (VAP).</p>
             <button>Toutes nos formations en VAE et VAP</button>
             <h3><span>La VAE :</span> reconnaissez votre expertise</h3>
@@ -20,6 +20,7 @@ const Vae = () => {
             Chez MDS Formations, nous évaluons vos acquis professionnels pour déterminer dans quelle mesure ils correspondent à nos programmes de formation. Ensuite, nous adaptons votre parcours de formation en conséquence.</p>
             <h3>Un chemin personnalisé <span>vers la réussite</span></h3>
             <p>La VAE et la VAP sont des moyens de personnaliser votre parcours de formation. Chez MDS Formations, nous croyons en l'importance de reconnaître et de valoriser l'expertise acquise par l'expérience professionnelle. Avec la VAE et la VAP, vous pouvez obtenir un diplôme reconnu qui témoigne de vos compétences dans le bien-être et l'esthétique, et vous ouvre de nouvelles opportunités professionnelles. N'hésitez pas à nous contacter pour en savoir plus sur ces processus et sur la manière dont nous pouvons vous accompagner dans votre démarche de reconnaissance et de valorisation de votre expérience.</p>
+            </div>
             <Accompa />
             <div className="home2">
             <h3>Obtenez des <span>diplômes reconnus</span> : Des formations de qualités à votre portée !</h3>
